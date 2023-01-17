f33662f28aaa5ed94cb05df95547b1c8f1787bc1
“sadik312” <sadikislam46@gmail.com>
2023-01-17 10:52:24 +0000

    list of goats

--- server/app.js
+++ server/app.js
@@ -2,12 +2,20 @@
 
 const express = require("express"); // access to Express Library
 
+const {goats, nextId } = require("./goats");
+
 const app = express(); // Make a very basic server using Express
 
 // Tell the app what kinds of requests to listen for (and how to handle them)
 
 app.get("/", (req, res) => { // req read info, res read info and send off
-    res.send("Hello World!");
+    res.json({
+        "message": "Hello, World!"
+    })
+})
+
+app.get("/goats", (req, res) => {
+    res.json(goats);
 })
 
 module.exports = app; // Make the server available to other files
---
+++ server/goats.js
@@ -0,0 +1,20 @@
+const goats = [       
+        {
+        "id": 1,
+        "name": "billy",
+         "age": 6,
+         "sex": "M"},
+        {
+        "id": 2,
+        "name": "Sam",
+         "age": 6,
+         "sex": "F"},
+        {
+        "id": 3,
+        "name": "Bob",
+         "age": 6,
+         "sex": "M"}];
+
+let nextId = 4;
+
+module.exports = { goats, nextId}
